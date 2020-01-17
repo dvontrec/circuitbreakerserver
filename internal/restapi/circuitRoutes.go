af575a3b703a6b536af1193148397ff583f9dfde
Dvontre Coleman <19761271+dvontrec@users.noreply.github.com>
2020-01-17 13:19:16 -0500

    Added dummy failure and recovery state

--- internal/restapi/circuitRoutes.go
+++ internal/restapi/circuitRoutes.go
@@ -2,7 +2,9 @@ package restapi
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // GetData returns simple data from the restapi
@@ -14,6 +16,22 @@ func (api *RESTAPI) GetData(w http.ResponseWriter, r *http.Request) {
 	if api.attemptsSinceFail >= 10 {
 		api.attemptsSincePass++
 		response = fmt.Sprintf("Server crashed\nattempts passed = %d\nattempts failed = %d", api.attemptsSinceFail, api.attemptsSincePass)
+		if api.attemptsSincePass >= 13 {
+			backUp := checkForRecovery()
+			if backUp {
+				api.attemptsSinceFail = 0
+				api.attemptsSincePass = 0
+			}
+		}
 	}
 	fmt.Fprintf(w, response)
+}
+
+func checkForRecovery() bool {
+	rand.Seed(time.Now().UnixNano())
+	rNum := rand.Intn(21)
+	if rNum >= 17 {
+		return true
+	}
+	return false
 }
